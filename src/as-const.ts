59e8baf7513d527a8922190a99517386b2df40ae
ikd-gifu <oki19ki@gmail.com>
2026-02-08 09:33:46 +0900

    ss9 52 Nullable Types

--- src/as-const.ts
+++ src/as-const.ts
@@ -8,10 +8,22 @@ let nickname = 'Ham' as const;
 // nickname = 'Hamtaro'; // 型 '"Hamtaro"' を型 '"Ham"' に割り当てることはできません
 nickname = 'Ham';
 
-let profile = {
+let profile = { // constアサーションで値のリテラル化・readonly 化
   name: 'Atsushi',
   height: 178
 } as const;
+
+// let profile = { // constアサーションは無意味
+//   name: string,
+//   height: number
+// } as const;
+
+// 以下のパターンはあり
+// 型として string/number を表したい
+// const profile: { name: string; height: number } = {
+//   name: 'Atsushi',
+//   height: 178,
+// };
 
 // as const を使うとオブジェクトの全てのプロパティが読み取り専用になる
 // nameが文字列リテラル型"Atsushi"、heightが数値リテラル型178になっている
---
+++ src/nullable-type.ts
@@ -0,0 +1,18 @@
+export {};
+// 設定したい値が確定していない時に使う型: Nullable Types
+
+// let profile: { name: string; age: number } = {
+//   name: 'Ham',
+//   age: null // 型 'null' を型 'number' に割り当てることはできません。ts(2322)
+// nullable-type.ts(4, 30): 予期された型は、型 '{ name: string; age: number; }' 
+// に対してここで宣言されたプロパティ 'age' から取得されています
+};
+// tsconfig.jsonで"strictNullChecks": false,を設定するとエラーが消える
+// ただし、他でもnullが許容されてしまう（nullableな状態）ため推奨されない
+// profile = null;
+
+// {}で型リテラルを定義
+let profile: { name: string; age: number | null } = { // union型でnullを許容
+  name: 'Ham',
+  age: null
+};
